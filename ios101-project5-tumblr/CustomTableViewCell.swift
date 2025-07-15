23b521db6ed88abd9a67efe3472d15777cfcbdfe
dndangg <144980612+dndangg@users.noreply.github.com>
2025-07-15 05:09:38 -0500

    done

---
+++ ios101-project5-tumblr/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Dylan Dang on 7/15/25.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
